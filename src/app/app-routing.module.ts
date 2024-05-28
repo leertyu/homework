acafc55aee49fd2bdccdf97e98de6cd836f316a0
DESKTOP-BK567Q3\neWsEasoN <thanapong.tienniem@gmail.com>
2024-05-28 11:40:14 +0700

    add routing for score page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/common/login.component';
 import { QuestionComponent } from './pages/questions/question/question.component';
 import { CategoriesComponent } from './pages/questions/categories/categories.component';
+import { ScoreComponent } from './pages/questions/score/score.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'question', component: QuestionComponent },
   { path: 'categories', component: CategoriesComponent },
+  { path: 'score', component: ScoreComponent },
 ];
 
 @NgModule({
