f4cc0c4ad69dc88053e6a146a95743efe0a51651
DESKTOP-BK567Q3\neWsEasoN <thanapong.tienniem@gmail.com>
2024-05-28 12:23:23 +0700

    show score

--- src/app/pages/questions/score/score.component.ts
+++ src/app/pages/questions/score/score.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../../services/local-storage.service';
 
 @Component({
   selector: 'app-score',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class ScoreComponent {
 
+  yourscore: string = '';
+  fullscore: string = '';
+
+  constructor(private router: Router, private localStorageService: LocalStorageService){ }
+
+  ngOnInit(){
+    const score = this.localStorageService.getItem('SCORE');
+    const scoreText = score !== null ? JSON.parse(score) : '';
+    console.log(scoreText);
+    this.yourscore = scoreText.score;
+    this.fullscore = scoreText.fullScore;
+  }
 }
